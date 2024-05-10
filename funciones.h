ebf3734bb3b26275fb37d56dc6b73d920dd883a0
Osma2005 <168149545+Osma2005@users.noreply.github.com>
2024-05-10 14:07:32 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,9 +1,4 @@
-#ifndef FUNCIONES_H
-#define FUNCIONES_H
-
 float calculoInteres(float saldo, float tasaInteres);
 float calcularTotalInteres(float monto, float interes, int tiempo);
 float calcularTotalMonto(float monto, int tiempo);
 void mostrarTabla(float monto, float interes, int tiempo);
-
-#endif  
